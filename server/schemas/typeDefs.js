bc3eb388d05a155abd897ac84b067ff2d6c8da1c
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-12 00:08:39 -0500

    changed a small syntax error in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,39 +1,46 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-        type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
-        bookCount: Int
-        savedBooks: [Book]!
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
 
-    type Book {
-        bookId: ID!
-        authors: [String]!
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+  type Book {
+    bookId: ID!
+    authors: [String]!
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Query {
-        me: User
-    }
+  type Query {
+    me: User
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook([authors]!, description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
-        removeBook(bookId: ID!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      author: String!
+      description: String!
+      title: String!
+      bookId: ID!
+      image: String!
+      link: String!
+    ): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
